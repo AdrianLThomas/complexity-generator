a8e24c3db5f02f553c4d6c5a78164a1b7bbf3a72
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-04-15 14:51:26 +0100

    test: add basic perf test from presentation

---
+++ test/performance/basic.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '5m',
+  thresholds: {
+    http_req_failed: ['rate<0.05'], // http errors should be less than 5%
+  },
+};
+
+export default function () {
+  http.get('https://complexity-api.adrian-thomas.com?complexity=20&error-rate=0.01'); // errors 1% of the time
+
+  sleep(0.1);
+}
