49b2dbe6c42a7c61fe836269ed327af8c85f17db
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-04-16 11:34:55 +0100

    feat: warm up

--- test/performance/basic.js
+++ test/performance/basic.js
@@ -2,11 +2,14 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '5m',
   thresholds: {
     http_req_failed: ['rate<0.05'], // http errors should be less than 5%
   },
+  stages: [
+    { duration: '30s', target: 10 }, // warm-up
+    { duration: '4m', target: 50 },  // test
+    { duration: '1m', target: 0 },  // ramp down
+  ]
 };
 
 export default function () {
